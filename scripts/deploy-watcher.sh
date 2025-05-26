841d77b863406be87e4c4074eb8463e8ada07ea6
Stevan H. Moeladi <stevan.moeladi@gmail.com>
2025-05-26 11:55:34 +0700

    chore: modify deploy watcher

--- scripts/deploy-watcher.sh
+++ scripts/deploy-watcher.sh
@@ -1,7 +1,16 @@
 #!/bin/env bash
 # echo "$PWD"
-pnpm build:apps
+# pnpm build:apps
+
+#
+pnpm cleanup
+pnpm install
+
+# build
+pnpm turbo run build
+
+# deploy
 pnpm --filter @yellow-mobile/watcher deploy --prod $PWD/dist/watcher
 
-mkdir -p $PWD/dist/watcher/prisma
-cp -R $PWD/packages/database/prisma/* $PWD/dist/watcher/prisma/
+# mkdir -p $PWD/dist/watcher/prisma
+# cp -R $PWD/packages/database/prisma/* $PWD/dist/watcher/prisma/
